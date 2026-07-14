9cf20008fca1e62404fc72bf98bc111da4423ed7
Kidist Mekuria <kidimeku2@gmail.com>
2026-07-14 12:04:01 +0300

    fic error

--- src/app/(team)/team-detail/_componenets/AppointForm.tsx
+++ src/app/(team)/team-detail/_componenets/AppointForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useRef, useState } from "react";
-import { IMAGES } from "../constant/theme";
+import { IMAGES } from "@/constant/theme";
 import { Dropdown } from "react-bootstrap";
 import Image from "next/image";
 import { useEmailService } from "@/constant/useEmailService";
